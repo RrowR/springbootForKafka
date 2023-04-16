b6e08b45306aa045e1960c4a60d45d966f594198
Memo <2415782224@qq.com>
2023-04-17 02:11:35 +0800

    使用了栈的版本

--- myKafka/src/main/java/com/example/transactionalformultipledatasource/aspect/MultipleDataSourceTransactionAspect.java
+++ myKafka/src/main/java/com/example/transactionalformultipledatasource/aspect/MultipleDataSourceTransactionAspect.java
@@ -1,26 +1,23 @@
 package com.example.transactionalformultipledatasource.aspect;
 
 import com.example.transactionalformultipledatasource.anno.MultipleDataSourceTransactional;
-import lombok.extern.slf4j.Slf4j;
+import javafx.util.Pair;
 import org.aspectj.lang.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionException;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Stack;
 
 /**
  * @author: Rrow
  * @date: 2023/4/3 23:41
  * Description:
  */
-@Slf4j
 @Component
 @Aspect
 public class MultipleDataSourceTransactionAspect {
@@ -28,8 +25,8 @@ public class MultipleDataSourceTransactionAspect {
     /**
      * 定义了一个线程安全的数组，里面存储的是栈，栈里面的对象是一个Map，Map的key是事务管理器，value是事务的状态
      */
-//    private static final ThreadLocal<Stack<Pair<DataSourceTransactionManager, TransactionStatus>>> THREAD_LOCAL = new ThreadLocal<>();
-    private static final ThreadLocal<Map<DataSourceTransactionManager, TransactionStatus>> THREAD_LOCAL = new ThreadLocal<>();
+   private static final ThreadLocal<Stack<Pair<DataSourceTransactionManager, TransactionStatus>>> THREAD_LOCAL = new ThreadLocal<>();
+    // private static final ThreadLocal<Map<DataSourceTransactionManager, TransactionStatus>> THREAD_LOCAL = new ThreadLocal<>();
 
     /**
      * 引入springboot上下文，用于获取事务管理器
@@ -63,16 +60,16 @@ public class MultipleDataSourceTransactionAspect {
         // 从注解里获取事务管理器的名字
         String[] transactionManagerNames = transactional.transactionManagers();
         // 创建一个栈
-//        Stack<Pair<DataSourceTransactionManager, TransactionStatus>> pairStack = new Stack<>();
-        HashMap<DataSourceTransactionManager, TransactionStatus> pairStack = new HashMap<>();
+       Stack<Pair<DataSourceTransactionManager, TransactionStatus>> pairStack = new Stack<>();
+        // HashMap<DataSourceTransactionManager, TransactionStatus> pairStack = new HashMap<>();
         for (String transactionManagerName : transactionManagerNames) {
             // 获取事务管理器
             DataSourceTransactionManager transactionManager = applicationContext.getBean(transactionManagerName, DataSourceTransactionManager.class);
             // 获取事务的状态
             TransactionStatus transactionStatus = transactionManager.getTransaction(defaultTransactionDefinition);
             // 将事务管理器和事务的状态放入栈中
-//            pairStack.push(new Pair<>(transactionManager, transactionStatus));
-            pairStack.put(transactionManager, transactionStatus);
+           pairStack.push(new Pair<>(transactionManager, transactionStatus));
+            // pairStack.put(transactionManager, transactionStatus);
         }
         // 将存入了事务管理器和事务状态的栈放入线程安全的数组中
         THREAD_LOCAL.set(pairStack);
@@ -83,26 +80,21 @@ public class MultipleDataSourceTransactionAspect {
      */
     @AfterReturning("pointCut()")
     public void afterReturning() {
-//        Stack<Pair<DataSourceTransactionManager, TransactionStatus>> pairStack = THREAD_LOCAL.get();
-        try {
-            Map<DataSourceTransactionManager, TransactionStatus> map = THREAD_LOCAL.get();
-            for (Map.Entry<DataSourceTransactionManager, TransactionStatus> entry : map.entrySet()) {
-                DataSourceTransactionManager mapKey = entry.getKey();
-                TransactionStatus mapValue = entry.getValue();
-                mapKey.commit(mapValue);
-            }
-            THREAD_LOCAL.remove();
-        } catch (TransactionException e) {
-            log.error("提交事务失败", e);
-        }
+       Stack<Pair<DataSourceTransactionManager, TransactionStatus>> pairStack = THREAD_LOCAL.get();
+        // Map<DataSourceTransactionManager, TransactionStatus> map = THREAD_LOCAL.get();
+        // for (Map.Entry<DataSourceTransactionManager, TransactionStatus> entry : map.entrySet()) {
+        //     DataSourceTransactionManager mapKey = entry.getKey();
+        //     TransactionStatus mapValue = entry.getValue();
+        //     mapKey.commit(mapValue);
+        // }
 
         // 遍历栈，提交事务
-//        while (!pairStack.isEmpty()) {
-//            // 弹出栈顶的事务管理器和事务状态
-//            Pair<DataSourceTransactionManager, TransactionStatus> pair = pairStack.pop();
-//            // 提交事务
-//            pair.getKey().commit(pair.getValue());
-//        }
+       while (!pairStack.isEmpty()) {
+           // 弹出栈顶的事务管理器和事务状态
+           Pair<DataSourceTransactionManager, TransactionStatus> pair = pairStack.pop();
+           // 提交事务
+           pair.getKey().commit(pair.getValue());
+       }
 
         // 移除数组里的栈
         THREAD_LOCAL.remove();
@@ -111,29 +103,21 @@ public class MultipleDataSourceTransactionAspect {
     // 在方法抛出异常后，回滚事务
     @AfterThrowing("pointCut()")
     public void afterThrowing() {
-//        Stack<Pair<DataSourceTransactionManager, TransactionStatus>> pairStack = THREAD_LOCAL.get();
-        try {
-            Map<DataSourceTransactionManager, TransactionStatus> map = THREAD_LOCAL.get();
-            for (Map.Entry<DataSourceTransactionManager, TransactionStatus> entry : map.entrySet()) {
-                DataSourceTransactionManager mapKey = entry.getKey();
-                TransactionStatus mapValue = entry.getValue();
-                mapKey.rollback(mapValue);
-            }
-            THREAD_LOCAL.remove();
-        } catch (TransactionException e) {
-            log.error("事务回滚失败", e);
-        }
-
-        // 遍历栈，回滚事务
-//        while (!pairStack.isEmpty()) {
-//            // 弹出栈顶的事务管理器和事务状态
-//            Pair<DataSourceTransactionManager, TransactionStatus> pair = pairStack.pop();
-//            // 回滚事务
-//            pair.getKey().rollback(pair.getValue());
-//        }
-
-        // 移除数组里的栈
-        THREAD_LOCAL.remove();
+       Stack<Pair<DataSourceTransactionManager, TransactionStatus>> pairStack = THREAD_LOCAL.get();
+        // Map<DataSourceTransactionManager, TransactionStatus> map = THREAD_LOCAL.get();
+//         for (Map.Entry<DataSourceTransactionManager, TransactionStatus> entry : map.entrySet()) {
+//             DataSourceTransactionManager mapKey = entry.getKey();
+//             TransactionStatus mapValue = entry.getValue();
+//             mapKey.rollback(mapValue);
+//         }
+//
+//         遍历栈，回滚事务
+       while (!pairStack.isEmpty()) {
+           // 弹出栈顶的事务管理器和事务状态
+           Pair<DataSourceTransactionManager, TransactionStatus> pair = pairStack.pop();
+           // 回滚事务
+           pair.getKey().rollback(pair.getValue());
+       }
     }
 
 }
